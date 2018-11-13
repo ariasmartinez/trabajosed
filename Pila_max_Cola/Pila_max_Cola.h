67fc708fc8e24910dbe380a306a4714b86180c99
Celia Arias <ariasmartinez@correo.ugr.es>
2018-11-13 11:12:33 +0100

    Añado métodos en el .h según pila_min_lista.h

---
+++ Pila_max_Cola/Pila_max_Cola.h
@@ -0,0 +1,36 @@
+#ifndef __Pila_max_Cola_H__
+#define __Pila_max_Cola_H__
+
+typedef struct{
+  int ele; ///<elemento a almacenar
+  int minimo; ///<el minimo
+}elemento;
+
+class Pila_max_Cola{
+private:
+    Cola<elemento> datos;
+}
+
+public:
+  
+  Pila_max_Cola();
+
+  Pila_max_Cola(Cola<elementos> dat);
+
+  Pila_max_Cola(const Pila_max_Cola & otra);
+
+  ~Pila_max_Cola();
+
+  Pila_max_Cola& operator= (const Pila_max_Cola& otra);
+
+  bool vacia() const;
+
+  elemento & tope();
+
+  const elemento & tope () const;
+
+  void poner(const int & elem);
+
+  void quitar();
+
+  int num_elementos() const;
