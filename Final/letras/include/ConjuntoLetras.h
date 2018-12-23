64ef438823e469211ffe8f5d57217f9c0a784c7c
Lucía Salamanca <luciasalamanca@github.com>
2018-12-23 21:48:09 +0100

    Añado dos cabeceras de método en ConjuntoLetras

--- Final/letras/include/ConjuntoLetras.h
+++ Final/letras/include/ConjuntoLetras.h
@@ -21,6 +21,22 @@ public:
   */
   ConjuntoLetras(){}
 
+  /**
+  @brief Lee de un flujo de entrada un conjunto de letras
+  @param is:flujo de entrada
+  @param conj: el objeto donde se realiza la lectura.
+  @return el flujo de entrada
+  **/
+  friend istream & operator>>(istream & is,ConjuntoLetras & conj);
+  /**
+  @brief Escribe en un flujo de salida un conjunto de letras
+  @param os:flujo de salida
+  @param let: el objeto ConjuntoLetras que se escribe
+  @return el flujo de salida
+  **/
+  friend ostream & operator<<(ostream & os, const ConjuntoLetras &conj);
+
+
 };
 
 #endif
